54102785f88bcaa3cae8c344f31032d4dfa8537b
DavideLopez <davidelopez7@gmail.com>
2022-07-19 20:35:57 +0200

    aggiunto stile e fine

---
+++ app.js
@@ -0,0 +1,11 @@
+// domanda nome utente
+const nome = prompt('come ti chiami?');
+
+// DOMANDA COGNOME
+const cognome = prompt('quale è il tuo cognome?');
+
+// DOMANDA COLORE
+const colore = prompt('quale è il tuo colore preferito?');
+
+// RISULTATO
+title.innerHTML = nome + cognome + colore + 21;
